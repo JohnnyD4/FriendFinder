d9cdcceeda90248e103614a1173625f2b146e793
John Davis <johnsdavis95@gmail.com>
2017-06-26 18:38:03 -0400

    Added css to localhost

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -14,19 +14,31 @@ var publicHome = path.join(__dirname, '../public/home.html');
 
 var publicSurvey = path.join(__dirname, '../public/survey.html');
 
+var publicStyle = path.join(__dirname, '../public/style.css');
+
 app.get("/", function(req, res) {
 	
 	fs.readFile(publicHome, "utf8", function (err, data) {
 			// if (err) throw err;
 
-		// res.writeHead(200, {"Content-type": "text/html"});
-		res.send(data);
+		res.writeHead(200, {"Content-type": "text/html"});
+		res.end(data);
+	})
+})
+
+app.get("/style.css", function(req, res) {
+	
+	fs.readFile(publicStyle, "utf8", function (err, data) {
+			// if (err) throw err;
+
+		res.writeHead(200, {"Content-type": "text/css"});
+		res.end(data);
 	})
 })
 
 app.listen(PORT, function() {
 	console.log(PORT);
 	// console.log(fileName);
-	// console.log();
+	console.log(publicHome);
 
 })
